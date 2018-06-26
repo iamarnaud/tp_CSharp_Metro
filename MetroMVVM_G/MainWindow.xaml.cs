b0bb31594c11585b9f27c7918467fad1231677ac
Arnaud <arnaud.voiron@gmail.com>
2018-06-26 15:29:47 +0200

    personnalisation des pins et de la carte

--- MetroMVVM_G/MainWindow.xaml.cs
+++ MetroMVVM_G/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using Newtonsoft.Json;
 using MaBibliotheque;
 using Microsoft.Maps.MapControl.WPF;
+using System.Globalization;
 
 namespace MetroMVVM
 {
@@ -29,34 +30,35 @@ namespace MetroMVVM
             InitializeComponent();
         }
 
-        private String Xentré { get; set; } // longitude
-        private String Yentré { get; set; } // latitude
-        private String Rentré { get; set; } // rayon
+        private String Xentre { get; set; } // longitude
+        private String Yentre { get; set; } // latitude
+        private String Rentre { get; set; } // rayon
 
         public void Button_Click(object sender, RoutedEventArgs e)
 
         {
-            Xentré = Lon.Text; // Lat
-            Yentré = Lat.Text; // Lon
-            Rentré = Dist.Text; 
+            Xentre = Lon.Text; // Lon
+            Yentre = Lat.Text; // Lat
+            Rentre = Dist.Text;
 
             try
             {
-                AppelApi metroMvvmApi = new AppelApi("http://data.metromobilite.fr/api/linesNear/json?x=" + Xentré + "&y=" + Yentré + "&dist=" + Rentré + "&details=true");
+                AppelApi metroMvvmApi = new AppelApi("http://data.metromobilite.fr/api/linesNear/json?x=" + Xentre + "&y=" + Yentre + "&dist=" + Rentre + "&details=true");
 
                 List<Lines> lines = JsonConvert.DeserializeObject<List<Lines>>(metroMvvmApi.responseFromServer);
                 List<Lines> lineSansDoublons = lines.GroupBy(n => n.name).Select(x => x.First()).ToList();
-
 
                 /* itération ligne par ligne pour récupérer infos dans Lines
                 * soit lines soit lineSansDoublons
                 * et ajout des pins sur la carte
                 */
 
+                YouAreHere(Yentre, Xentre);
+
                 foreach (Lines line in lineSansDoublons)
                 {
                     Result.Items.Add(line.name);
-                    StopPin(line.lat, line.lon);
+                    StopPin(line.lat, line.lon, line.name);
                 }
             }
             catch (Exception ex)
@@ -66,7 +68,7 @@ namespace MetroMVVM
             }
         }
 
-        public void StopPin(Double Lat, Double Lon)
+        public void StopPin(Double Lat, Double Lon, String name)
         {
             //Convert the mouse coordinates to a location on the map
             // Création d'un nouveau lieu avec lat et lon
@@ -79,6 +81,40 @@ namespace MetroMVVM
 
             // Adds the pushpin to the map.
             Map.Children.Add(pin);
+            //Ajout du nom des arrets sur les pins
+            ToolTipService.SetToolTip(pin, $"{name}");
+        }
+
+        // pin de localisation selon les infos rentrées par l'utilisateur
+        public void YouAreHere(String latHere, String lonHere)
+        {
+            // On converti un double en string et en mettant un . à la place de la ,
+            Double latHereConverted = Convert.ToDouble(latHere, new CultureInfo("en-GB"));
+            Double lonHereConverted = Convert.ToDouble(lonHere, new CultureInfo("en-GB"));
+
+            //Convert the mouse coordinates to a location on the map
+            //Création d'un nouveau lieu avec lat et lon
+            Location pinLocation = new Location(latHereConverted, lonHereConverted);
+
+            // The pushpin to add to the map.
+            // Pushpin est la classe définie par Maps
+            Pushpin pinHere = new Pushpin();
+            pinHere.Location = pinLocation;
+
+            //Ajout d'un message sur le pin de géolocalisation
+            ToolTipService.SetToolTip(pinHere, "You are here !");
+
+            // Changement de la couleur du pin
+            pinHere.Background = new SolidColorBrush(Color.FromRgb(33, 23, 125));
+
+            // Adds the pushpin to the map.
+            Map.Children.Add(pinHere);
+            // Recentrer la map selon les infos rentrées par l'utilisateur
+            Map.Center = pinLocation;
+            // Zoomer map sur nouvelle zone
+            Map.ZoomLevel = 17;
+            
+
         }
 
         public void Button_Reset(object sender, RoutedEventArgs e)
@@ -86,6 +122,5 @@ namespace MetroMVVM
             Result.Items.Clear();
             Map.Children.Clear();
         }
-
     }
 }
